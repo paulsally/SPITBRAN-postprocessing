bdc84e9b109d9bd3ecfd0b41176b3bf34a3b9db6
Sara Polselli <polselli@hpc-fe1.lamma.it>
2025-02-04 15:19:25 +0000

    changed function name get_values_in_point_with_time_given_month to clearify that it works with dates of the form YYYYMM

--- lib/my_nc_utilities.py
+++ lib/my_nc_utilities.py
@@ -58,7 +58,7 @@ def get_lat_lon_idx(p_ds, p_latitude, p_longitude):
     return p_lat_idx, p_lon_idx
 
 
-def get_values_in_point_with_time(
+def get_values_in_point_with_time_given_month(
     p_ds_type,
     p_data_base_dir,
     p_target_date,
@@ -70,7 +70,7 @@ def get_values_in_point_with_time(
     p_var_d=False,
 ):
     """
-    Searches the MITgcm-BFM data directory for files related to the target date (a month if format YYYYMM) and extracts the variable time series values for the given depth, lat, and lon.
+    Searches the MITgcm-BFM data directory for files related to the target date (a month in format YYYYMM) and extracts the variable time series values for the given depth, lat, and lon.
        
     Parameters
     ----------
@@ -157,3 +157,89 @@ def get_values_in_point_with_time(
         return x, y, var_long_name, var_units
     elif p_ds_type == "m":
         return x, y, y_d
+    
+def get_values_map_specific_day(
+    p_ds_type,
+    p_data_base_dir,
+    p_target_date,
+    p_var,
+    p_var_fn_mapped,
+    p_depth_index,
+    p_var_d=False,
+):
+    """
+    Searches the MITgcm-BFM data directory for files related to the target date (a month if format YYYYMM) and extracts the variable time series values for the given depth, lat, and lon.
+       
+    Parameters
+    ----------
+    p_ds_type :                 str
+                                String corresponding to the type of dataset (c for CMEMS, m for MITgcm-BFM).
+    p_data_base_dir :           str
+                                String corresponding to the path where data files are located.
+    p_target_date :             str
+                                Target date in the format YYYYMM.
+    p_var :                     str
+                                Varibale to extract from the data files.
+    p_var_fn_mapped :           str
+                                Mapped variable name as per the config file (as it shows in data filenames).
+    p_latitude :                float
+                                Degrees North latitude.
+    p_longitude :               float
+                                Degrees East longitude.
+    p_depth_index :             int
+                                Depth index (indicates the layer).
+    p_var_d :                   bool
+                                Flag to compute average daily variable values.
+
+
+    Returns
+    -------
+    x :                         list
+                                The values to plot on the x-axis.
+    y :                         list
+                                The values to plot on the y-axis.
+    y_d :                       list
+                                The average daily values to plot on the y-axis
+    var_long_name :             str
+                                The long name of the variable.
+    var_units :                 str
+                                The units of the variable.
+    """
+
+    x = []
+    y = []
+    y_d = []
+
+    matches = sorted(
+        my_sys_utilities.get_files_by_keystring_in_fn(
+            p_data_base_dir, 
+            p_ds_type,
+            p_var_fn_mapped,
+            p_target_date,
+        )
+    )
+
+    i = 0
+    for (item) in matches:
+        with nc.Dataset(item, "r") as ds:
+
+            if p_ds_type == "c":
+                var_units = ds.variables[p_var].units
+                var_long_name = ds.variables[p_var].long_name
+              
+            # Extract variable values for the given depth, lat, and lon (cell)
+            var_values = ds.variables[p_var][:, p_depth_index, :, :]
+            y.extend(var_values)
+
+            # Compute avarage daily variable values
+            if (p_var_d == True):
+                var_d = ds.variables[p_var][:, p_depth_index, lat_idx, lon_idx].mean(axis=0)
+                y_d.append(var_d)
+
+        i += 1
+
+    if p_ds_type == "c":
+        return x, y, var_long_name, var_units
+    elif p_ds_type == "m":
+        return x, y
+    
