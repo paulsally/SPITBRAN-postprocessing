c3d2c9bbd098fad9e1406a556a441725e62528e9
Sara Polselli <polselli@hpc-fe1.lamma.it>
2025-03-03 18:49:42 +0000

    Updated cmems download script python method

--- scripts/download_cmems_data-daily.py
+++ scripts/download_cmems_data-daily.py
@@ -7,19 +7,20 @@ from datetime import datetime, timedelta
 # Check if python script is running in interactive mode
 if getattr(sys, "ps1", None) is None:
     if len(sys.argv) == 3:
-        start_date = sys.argv[1]
-        end_date = sys.argv[2]
+        start_date_str = sys.argv[1]
+        end_date_str = sys.argv[2]
     else:
         exit("Missing arguments (start date, end date or both)")
 else:
-    start_date = input("Enter the start date in format YYYYMMDD") or "20130401"
-    end_date = input("Enter the end date in format YYYYMMDD") or "20130430"
+    start_date_str = input("Enter the start date in format YYYYMMDD") or "20130401"
+    end_date_str = input("Enter the end date in format YYYYMMDD") or "20130403"
 
 data_dir = rf"{Path(__file__).parent.parent}/DATA/CMEMS/TEST"
 
 # Define start and end dates
-start_date = datetime.strptime("20230101", "%Y%m%d")
-end_date = datetime.strptime("20230110", "%Y%m%d")
+start_date = datetime.strptime(start_date_str, "%Y%m%d")
+end_date = datetime.strptime(end_date_str, "%Y%m%d")
+# end_date = datetime.strptime("20230110", "%Y%m%d")
 # Initialize the looping variable
 current_date = start_date
 step = timedelta(days=1)
@@ -28,22 +29,20 @@ step = timedelta(days=1)
 files = ["cur", "sal", "ssh", "tem"]
 # files = ["ssh", "tem"]
 
-
-for var_fn in files:
-    while start_date <= current_date <= end_date:
+while start_date <= current_date <= end_date:
+    for var_fn in files:
         copernicusmarine.subset(
             dataset_id=f"med-cmcc-{var_fn}-rean-d",
             # variables=["thetao"],
-            # minimum_longitude=6.066406,
-            # maximum_longitude=12.12891,
-            # minimum_latitude=41.88515,
-            # maximum_latitude=44.50234,
+            minimum_longitude=6.083333,
+            maximum_longitude=12.16667,
+            minimum_latitude=41.89583,
+            maximum_latitude=44.53,
             start_datetime=f"{current_date}T00:00:00",
             end_datetime=f"{current_date}T23:00:00",
             # minimum_depth=1.0182366371154785,
             # maximum_depth=1.0182366371154785,
             output_directory=data_dir,
-            # output_name=f"cmems_{var_fn}-rean_d_{var_fn}",
+            output_filename=f"cmems_{var_fn}-rean_d_{current_date.strftime('%Y%m%d')}.nc",
         )
-        current_date += step
-    
+    current_date += step
--- scripts/download_cmems_data.py
+++ scripts/download_cmems_data.py
@@ -1,30 +1,35 @@
 import sys
 from pathlib import Path
 import copernicusmarine
+from datetime import datetime
 
 # Check if python script is running in interactive mode
 if getattr(sys, "ps1", None) is None:
     if len(sys.argv) == 3:
-        start_date = sys.argv[1]
-        end_date = sys.argv[2]
+        start_date_str = sys.argv[1]
+        end_date_str = sys.argv[2]
     else:
         exit("Missing arguments (start date, end date or both)")
 else:
-    start_date = input("Enter the start date in format YYYY-MM-DD")
-    end_date = input("Enter the end date in format YYYY-MM-DD")
+    start_date_str = input("Enter the start date in format YYYYMMDD")
+    end_date_str = input("Enter the end date in format YYYYMMDD")
 
-data_dir = rf"{Path(__file__).parent.parent}/DATA/CMEMS"
+start_date = datetime.strptime(start_date_str, "%Y%m%d")
+end_date = datetime.strptime(end_date_str, "%Y%m%d")
+
+data_dir = rf"{Path(__file__).parent.parent}/DATA/CMEMS/TEST/TO-FROM"
 
 copernicusmarine.subset(
     dataset_id="med-cmcc-tem-rean-d",
-    variables=["thetao"],
-    minimum_longitude=6.066406,
-    maximum_longitude=12.12891,
-    minimum_latitude=41.88515,
-    maximum_latitude=44.50234,
+    # variables=["thetao"],
+    minimum_longitude=6.083333,
+    maximum_longitude=12.16667,
+    minimum_latitude=41.89583,
+    maximum_latitude=44.53,
     start_datetime=f"{start_date}T00:00:00",
     end_datetime=f"{end_date}T23:00:00",
-    minimum_depth=1.0182366371154785,
-    maximum_depth=1.0182366371154785,
+    # minimum_depth=1.0182366371154785,
+    # maximum_depth=1.0182366371154785,
     output_directory=data_dir,
+    output_filename=f"cmems_tem-rean_d_{start_date.strftime('%Y%m%d')}-{end_date.strftime('%Y%m%d')}.nc",
 )
