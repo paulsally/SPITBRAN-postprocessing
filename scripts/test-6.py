960873be0d4dd6a12137ae78f8fa8283805bbf18
Sara Polselli <polselli@hpc-fe1.lamma.it>
2025-03-11 16:30:46 +0000

    WIP: script for plotting temp profile from insitu cmems obs

--- scripts/test-6.py
+++ scripts/test-6.py
@@ -12,21 +12,32 @@ from lib import my_nc_utilities
 
 
 # test_ds_w_m = nc.Dataset("/OCEANASTORE/progetti/spitbran2/2013/20130101/20130101_h-OGS--TEMP-MITgcmBFM-pilot8-b20130101_fc-v01.nc", 'r')
-test_ds_w_c = nc.Dataset("/home/polselli/SPITBRAN/DATA/CMEMS/rean-d/cmems_tem-rean_d_20120101.nc", 'r')
-test_ds_s_c = nc.Dataset("/home/polselli/SPITBRAN/DATA/CMEMS/rean-d/cmems_tem-rean_d_20120701.nc", 'r')
+test_ds_w_c_rean = nc.Dataset("/home/polselli/SPITBRAN/DATA/CMEMS/rean-d/cmems_tem-rean_d_20120101.nc", 'r')
+test_ds_s_c_rean = nc.Dataset("/home/polselli/SPITBRAN/DATA/CMEMS/rean-d/cmems_tem-rean_d_20120701.nc", 'r')
 test_ds_w_m = nc.Dataset("/OCEANASTORE/progetti/spitbran2/MITgcm_products/outputs/20120101/20120101_h-OGS--TEMP-MITgcmBFM-pilot8-b20120101_fc-v01.nc", 'r')
 test_ds_s_m = nc.Dataset("/OCEANASTORE/progetti/spitbran2/MITgcm_products/outputs/20120701/20120701_h-OGS--TEMP-MITgcmBFM-pilot8-b20120701_fc-v01.nc", 'r')
+test_ds_w_c_insitu = nc.Dataset("/home/polselli/SPITBRAN/DATA/CMEMS/insitu/INSITU_GLO_PHY_TS_DISCRETE_MY_013_001/cmems_obs-ins_glo_phy-temp-sal_my_easycora_irr_202411/mediterrane/2012/ECO_DMQCGL01_20120101_PR_CT.nc", 'r')
+test_ds_s_c_insitu = nc.Dataset("/home/polselli/SPITBRAN/DATA/CMEMS/insitu/INSITU_GLO_PHY_TS_DISCRETE_MY_013_001/cmems_obs-ins_glo_phy-temp-sal_my_easycora_irr_202411/mediterrane/2012/ECO_DMQCGL01_20120701_PR_CT.nc", 'r')
 
 
 # Fixed time, lat, lon
-depth_w_c = test_ds_w_c.variables['depth'][:]
+depth_w_c_rean = test_ds_w_c_rean.variables['depth'][:]
+depth_w_c_insitu = test_ds_w_c_insitu.variables['DEPH'][0, :]
+depth_s_c_insitu = test_ds_s_c_insitu.variables['DEPH'][0, :]
 depth_w_m = test_ds_w_m.variables['depth'][:]
 
 time = 0
 
-lat_idx_c, lon_idx_c = my_nc_utilities.get_lat_lon_idx(test_ds_w_c, spitbran_config.cfg_latitude, spitbran_config.cfg_longitude)
-thetao_w_c_profile = test_ds_w_c.variables['thetao'][time, :, lat_idx_c, lon_idx_c]
-thetao_s_c_profile = test_ds_s_c.variables['thetao'][time, :, lat_idx_c, lon_idx_c]
+lat_idx_c_rean, lon_idx_c_rean = my_nc_utilities.get_lat_lon_idx(test_ds_w_c_rean, spitbran_config.cfg_latitude, spitbran_config.cfg_longitude)
+thetao_w_c_rean_profile = test_ds_w_c_rean.variables['thetao'][time, :, lat_idx_c_rean, lon_idx_c_rean]
+thetao_s_c_rean_profile = test_ds_s_c_rean.variables['thetao'][time, :, lat_idx_c_rean, lon_idx_c_rean]
+
+lat_idx_c_insitu, lon_idx_c_insitu = my_nc_utilities.get_lat_lon_idx(test_ds_w_c_insitu, spitbran_config.cfg_latitude, spitbran_config.cfg_longitude)
+print("depth ", test_ds_w_c_insitu.variables['DEPH'].dimensions)
+print("temp ", test_ds_w_c_insitu.variables['TEMP'].dimensions)
+
+thetao_w_c_insitu_profile = test_ds_w_c_insitu.variables['TEMP'][0, :]
+thetao_s_c_insitu_profile = test_ds_s_c_insitu.variables['TEMP'][0, :]
 
 lat_idx_m, lon_idx_m = my_nc_utilities.get_lat_lon_idx(test_ds_w_m, spitbran_config.cfg_latitude, spitbran_config.cfg_longitude)
 thetao_w_m_profile = test_ds_w_m.variables['thetao'][:, :, lat_idx_m, lon_idx_m].mean(axis=0)
@@ -36,24 +47,51 @@ thetao_s_m_profile = test_ds_s_m.variables['thetao'][:, :, lat_idx_m, lon_idx_m]
 depth_w_m_units = test_ds_w_m.variables['depth'].units
 thetao_w_m_units = test_ds_w_m.variables['thetao'].units
 
-plt.figure(figsize=(12, 6))
-plt.plot(
-    thetao_w_c_profile, 
-    depth_w_c,
+#plt.figure(figsize=(12, 6))
+
+fig, ax1 = plt.subplots(figsize=(6, 8))
+
+
+ax1.plot(
+    thetao_w_c_rean_profile, 
+    depth_w_c_rean,
     marker=",", 
     linestyle="solid", 
     color=spitbran_config.cfg_colours["c-rean"]["seasonal"]["winter"],
     label="C Rean Winter",
 )
-plt.plot(
-    thetao_s_c_profile, 
-    depth_w_c,
+ax1.plot(
+    thetao_s_c_rean_profile, 
+    depth_w_c_rean,
     marker=",", 
     linestyle="solid", 
     color=spitbran_config.cfg_colours["c-rean"]["seasonal"]["summer"],
     label="C Rean Summer",
 )
-plt.plot(
+ax1.invert_yaxis()
+
+
+ax2 = ax1.twinx()
+ax2.plot(
+    thetao_w_c_insitu_profile, 
+    depth_w_c_insitu,
+    marker=",", 
+    linestyle="solid", 
+    # color=spitbran_config.cfg_colours["c-rean"]["seasonal"]["winter"],
+    color="y",
+    label="C Insitu Winter",
+)
+ax2.plot(
+    thetao_s_c_insitu_profile, 
+    depth_s_c_insitu,
+    marker=",", 
+    linestyle="solid", 
+    # color=spitbran_config.cfg_colours["c-rean"]["seasonal"]["summer"],
+    color="r",
+    label="C Insitu Summer",
+)
+
+ax1.plot(
     thetao_w_m_profile, 
     depth_w_m,
     marker=",", 
@@ -61,7 +99,7 @@ plt.plot(
     color=spitbran_config.cfg_colours["m"]["seasonal"]["winter"],
     label="M Winter",
 )
-plt.plot(
+ax1.plot(
     thetao_s_m_profile, 
     depth_w_m,
     marker=",", 
@@ -74,12 +112,21 @@ plt.gca().invert_yaxis()
 
 
 # **Set labels dynamically using dataset units**
+# ax1.set_xlabel(f"Temperature ({thetao_w_m_units})")
+# ax1.set_ylabel(f"Depth ({depth_w_m_units})")
 plt.xlabel(f"Temperature ({thetao_w_m_units})")
 plt.ylabel(f"Depth ({depth_w_m_units})")
 
 # Add title and legend
 plt.title("Temperature Profile from NetCDF Data")
-plt.legend()
+# plt.legend()
+# Get handles and labels from both axes
+lines_ax1, labels_ax1 = ax1.get_legend_handles_labels()
+lines_ax2, labels_ax2 = ax2.get_legend_handles_labels()
+
+# Combine legends and add to the plot
+plt.legend(lines_ax1 + lines_ax2, labels_ax1 + labels_ax2, loc="lower right")
+
 
 # Show grid
 plt.grid()
